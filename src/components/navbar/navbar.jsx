89e95c6f203cd96ce610adeb4ac99a3379a700d9
Daniel <mrtzdiazdaniel@gmail.com>
2024-07-16 16:57:49 -0600

    Change .map of navbar pages

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,38 +1,21 @@
 const Navbar = function () {
-
-    const pages = [
-        {
-            name: "Inicio",
-            url: "/index.html",
-        },
-        {
-            name: "Costos",
-            url: "/html/costos.html",
-        },
-        {
-            name: "Productos",
-            url: "/html/productos.html",
-        },
-        {
-            name: "Ingresos",
-            url: "/html/ingresos.html",
-        },
-        {
-            name: "Egresos",
-            url: "/html/egresos.html",
-        }]
-
+        //<img src="https://www.homi.com.mx/cdn/shop/files/Logo.png?v=1720505122&width=140" alt="HOMI" />
     return (
         <header className="sticky top-0">
             <nav className="bg-zinc-950  min-h-20 flex flex-row justify-between items-center">
                 <div className="basis-1/6">
                     <a href="../index.html">
-                        <img src="https://www.homi.com.mx/cdn/shop/files/Logo.png?v=1720505122&width=140" alt="HOMI" />
+                    LOGO
                     </a>
                 </div>
                 <section className="text-2xl text-center grid grid-cols-5 mx-10 basis-5/6">
-                    {pages.map(page => {
-                        const { name, url } = page
+                    {
+                        [['Inicio', '/index.html'],
+                        ['Costos', '/html/costos.html'],
+                        ['Productos', '/html/productos.html'],
+                        ['Ingresos', '/html/ingresos.html'],
+                        ['Egresos', '/html/egresos.html']
+                    ].map(([name, url]) => {
                         const state = (location.href.includes(url)) ? "text-yellow-400" : "text-white"
                         return (
                             <strong key={url}>
@@ -40,7 +23,7 @@ const Navbar = function () {
                             </strong>
                         )
                     })
-                    }
+                }
                 </section>
             </nav>
         </header>
