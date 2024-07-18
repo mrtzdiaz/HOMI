79ef28daf626a7af91a2938d66d70c19aa0cd6cb
Daniel <mrtzdiazdaniel@gmail.com>
2024-07-17 21:16:45 -0600

    Style and add responsive of page costos

--- src/components/materiaPrima/materiaPrima.jsx
+++ src/components/materiaPrima/materiaPrima.jsx
@@ -1,11 +1,22 @@
-const MateriaPrima = function({nombre, costo, unidad, almacen}){
-    return(
-        <tr>
-            <td>{nombre}</td>
-            <td>{costo}</td>
-            <td>{unidad}</td>
-            <td>{almacen}</td>
-        </tr>
+const MateriaPrima = function ({ nombre, costo, unidad, almacen }) {
+    return (
+        <div className='group/row table-row h-10 odd:bg-gray-100 even:bg-gray-50 hover:bg-sky-100'>
+            <div className='table-cell align-middle'>{nombre}</div>
+            <div className='table-cell align-middle'>${Number.parseFloat(costo).toFixed(2)}</div>
+            <div className='table-cell align-middle'>{unidad}</div>
+            <div className='table-cell align-middle'>{almacen}</div>
+            <div className='table-cell align-middle'>
+                <div className="flex justify-center invisible group-hover/row:visible">
+                <a href="" className="group/edit flex items-center justify-center hover:bg-black rounded-xl w-11/12 md:w-10/12 lg:w-7/12">
+                <p className="text-sm text-black group-hover/edit:text-white">Editar</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4 fill-black group-hover/edit:fill-white">
+                        <path d="M13.488 2.513a1.75 1.75 0 0 0-2.475 0L6.75 6.774a2.75 2.75 0 0 0-.596.892l-.848 2.047a.75.75 0 0 0 .98.98l2.047-.848a2.75 2.75 0 0 0 .892-.596l4.261-4.262a1.75 1.75 0 0 0 0-2.474Z" />
+                        <path d="M4.75 3.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h6.5c.69 0 1.25-.56 1.25-1.25V9A.75.75 0 0 1 14 9v2.25A2.75 2.75 0 0 1 11.25 14h-6.5A2.75 2.75 0 0 1 2 11.25v-6.5A2.75 2.75 0 0 1 4.75 2H7a.75.75 0 0 1 0 1.5H4.75Z" />
+                    </svg>
+                </a>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/jsx/costos.jsx
+++ src/jsx/costos.jsx
@@ -12,30 +12,39 @@ root.render(
         <div className='grid place-content-center min-h-18 bg-black text-3xl text-white [text-shadow:_2.5px_2.5px_teal]'>
             <strong>Costos Básicos</strong>
         </div>
-        <div className='grid place-content-center mx-20 mt-5'>
-            <table className='w-full table-fixed border rounded-2xl border-slate-200 box-shadow text-center'>
-                <thead>
-                    <tr className='min-h-80 bg-blue-200'>
-                        <th>Nombre</th>
-                        <th>Costo</th>
-                        <th>Unidad</th>
-                        <th>Almacen</th>
-                        <th>
-                            <a href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-slate-300">
-                                    <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
-                                </svg>
-                            </a>
-                        </th>
-                    </tr>
-                </thead>
-                    <tbody>
+        <div className='flex justify-center my-10 drop-shadow-2xl'>
+            <div className='table rounded-lg  w-11/12 md:w-9/12 lg:w-8/12 text-center bg-white'>
+                <div className='table-header-group'>
+                    <div className='table-row font-bold bg-black h-10 text-white'>
+                        <div className='table-cell md:w-3/12 align-middle rounded-tl-lg'>Nombre</div>
+                        <div className='table-cell md:w-2/12 align-middle'>Costo</div>
+                        <div className='table-cell md:w-2/12 align-middle'>Unidad</div>
+                        <div className='table-cell md:w-2/12 align-middle'>Almacén</div>
+                        <div className='table-cell md:w-2/12 lg:w-2/12 align-middle rounded-tr-lg'>
+                            <div className="group/add flex justify-center">
+                                <button href="" className="flex items-center justify-center bg-white group-hover/add:bg-cyan-600 rounded-xl w-11/12 md:w-10/12 lg:w-7/12">
+                                    <p className="text-sm text-black group-hover/add:text-white">Añadir</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4 fill-black group-hover/edit:fill-white">
+                                    <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+                                    </svg>
+
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="table-row-group">
+                    <MateriaPrima nombre='Daniel Martínez Díaz' costo='100' unidad='pza' almacen='1' />
                     <MateriaPrima nombre='Daniel' costo='100' unidad='pza' almacen='1' />
                     <MateriaPrima nombre='Daniel' costo='100' unidad='pza' almacen='1' />
                     <MateriaPrima nombre='Daniel' costo='100' unidad='pza' almacen='1' />
                     <MateriaPrima nombre='Daniel' costo='100' unidad='pza' almacen='1' />
-                    </tbody>
-            </table>
+                    <MateriaPrima nombre='Daniel' costo='100' unidad='pza' almacen='1' />
+                    <MateriaPrima nombre='Daniel' costo='100' unidad='pza' almacen='1' />
+                    <MateriaPrima nombre='Daniel' costo='100' unidad='pza' almacen='1' />
+                    <MateriaPrima nombre='Daniel' costo='100' unidad='pza' almacen='1' />
+                </div>
+            </div>
         </div>
     </section>
 );
