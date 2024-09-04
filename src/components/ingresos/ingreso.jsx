011243c4b7d5bd560e056d8d56bc82b56ccf8927
Daniel <mrtzdiazdaniel@gmail.com>
2024-09-04 13:10:38 -0600

    New Version of page Ingresos

--- src/components/ingresos/ingreso.jsx
+++ src/components/ingresos/ingreso.jsx
@@ -1,14 +1,44 @@
-const Ingreso = function ({fecha, canal, producto, cantidad, tipoVenta, precio, total}) {
-    const tiposVenta = {0: 'Tercero', 1: 'Tienda', 2: 'Final'}
+import { useState } from "react";
+
+const Ingreso = function ({ fecha, canal, producto, cantidad, tipoVenta, precio, total }) {
+    const tiposVenta = { 0: 'Tercero', 1: 'Tienda', 2: 'Final' }
+
+    const [showInfo, setShowInfo] = useState(false);
+    const showInfoClass = showInfo ? 'max-h-52' : 'max-h-0 md:max-h-14'
+
     return (
-        <div className="table-row odd:bg-gray-100 even:bg-gray-200 grid grid-cols-12 h-10 place-content-center text-center font-bold">
-            <div className="table-cell col-span-2">{fecha}</div>
-            <div className="table-cell col-span-2">{canal}</div>
-            <div className="table-cell col-span-2">{producto}</div>
-            <div className="table-cell col-span-1">{cantidad}</div>
-            <div className="table-cell col-span-2">{tiposVenta[tipoVenta]}</div>
-            <div className="table-cell col-span-1">{precio}</div>
-            <div className="table-cell col-span-2">{total}</div>
+        <div onClick={() => { setShowInfo(!showInfo) }} className={`table-row odd:bg-gray-100 even:bg-gray-200`}>
+            <div className="grid grid-cols-4 md:hidden place-content-center overflow-hidden text-center">
+                <div className="table-cell col-span-2">
+                    <div className="block md:hidden font-bold">Fecha</div>
+                    {fecha}</div>
+                <div className="table-cell col-span-2">
+                    <div className="block md:hidden font-bold">Canal</div>
+                    {canal}</div>
+            </div>
+            <div className={`${showInfoClass} transition-max-height grid grid-cols-4 md:grid-cols-12 place-content-center overflow-hidden text-center md:font-bold`}>
+                <div className="table-cell col-span-2 hidden md:block">
+                    <div className="block md:hidden font-bold">Fecha</div>
+                    {fecha}</div>
+                <div className="table-cell col-span-2 hidden md:block">
+                    <div className="block md:hidden font-bold">Canal</div>
+                    {canal}</div>
+                <div className="table-cell col-span-2">
+                    <div className="block md:hidden font-bold">Producto</div>
+                    {producto}</div>
+                <div className="table-cell col-span-2 md:col-span-1">
+                    <div className="block md:hidden font-bold">Cantidad</div>
+                    {cantidad}</div>
+                <div className="table-cell col-span-2">
+                    <div className="block md:hidden font-bold">Tipo</div>
+                    {tiposVenta[tipoVenta]}</div>
+                <div className="table-cell col-span-2 md:col-span-1">
+                    <div className="block md:hidden font-bold">Costo</div>
+                    ${precio}</div>
+                <div className="table-cell col-span-4 md:col-span-2">
+                    <div className="block md:hidden font-bold">Total</div>
+                    ${total}</div>
+            </div>
         </div>
     )
 }
--- src/components/ingresos/tablaIngresos.jsx
+++ src/components/ingresos/tablaIngresos.jsx
@@ -9,8 +9,8 @@ const TablaIngresos = function () {
     const ingresos = ingresosJSON ? JSON.parse(ingresosJSON) : [];
 
     const [showAddForm, setShowAddForm] = useState(false);
-    const isShowed = showAddForm ? 'max-h-14' : 'max-h-0 overflow-hidden';
-    const controlForm = showAddForm ? 'translate-x-full' : '';
+    const isShowed = showAddForm ? 'sm:max-h-40 md:max-h-14' : 'max-h-0 overflow-hidden';
+    const controlForm = showAddForm ? 'translate-x-1/3 sm:translate-x-3/4 md:translate-x-full' : '';
 
     const [cantidad, setCantidad] = useState('0');
     const [nombreProducto, setNombreProducto] = useState(productos[0].nombre);
@@ -50,30 +50,38 @@ const TablaIngresos = function () {
 
 
     return (
-        <section className="flex justify-center mx-24 my-10">
+        <section className="flex justify-center mx-10 md:mx-24 my-10">
             <div className="table w-full">
                 <div className="table-header-group">
                     <div className="table-row grid grid-cols-12 h-10 place-content-center rounded-t-lg bg-blackH text-center text-white font-bold">
-                        <div className="table-cell col-span-2">Fecha</div>
-                        <div className="table-cell col-span-2">Canal</div>
-                        <div className="table-cell col-span-2">Producto</div>
-                        <div className="table-cell col-span-1">Cantidad</div>
-                        <div className="table-cell col-span-2">Tipo de Venta</div>
-                        <div className="table-cell col-span-1">Precio</div>
-                        <div className="table-cell col-span-2">Total</div>
+                        <div className="table-cell block md:hidden col-span-12">Registros</div>
+                        <div className="table-cell hidden md:block col-span-2">Fecha</div>
+                        <div className="table-cell hidden md:block col-span-2">Canal</div>
+                        <div className="table-cell hidden md:block col-span-2">Producto</div>
+                        <div className="table-cell hidden md:block col-span-1">Cantidad</div>
+                        <div className="table-cell hidden md:block col-span-2">Tipo</div>
+                        <div className="table-cell hidden md:block col-span-1">Precio</div>
+                        <div className="table-cell hidden md:block col-span-2">Total</div>
                     </div>
                     {
-                        ingresos.map((ingreso) =>{
-                            return(
-                                <Ingreso fecha={ingreso.fecha} canal={ingreso.canal} producto={ingreso.producto} cantidad={ingreso.cantidad} tipoVenta={ingreso.tipoVenta} precio={ingreso.precio} total={ingreso.total}/>
+                        ingresos.map((ingreso) => {
+                            return (
+                                <Ingreso fecha={ingreso.fecha} canal={ingreso.canal} producto={ingreso.producto} cantidad={ingreso.cantidad} tipoVenta={ingreso.tipoVenta} precio={ingreso.precio} total={ingreso.total} />
                             )
                         })
                     }
-                    <form onSubmit={saveData} className={`transition-max-height duration-300 delay-100 ${isShowed} table-row grid grid-cols-12 place-content-center bg-gray-50 text-center font-bold`}>
-                        <div className="table-cell col-span-2"><input name="fecha" type="date" className="p-1 my-2 w-3/4 rounded-lg text-center" /></div>
-                        <div className="table-cell col-span-2"><input name="canal" type="text" className="p-1 my-2 w-3/4 rounded-lg text-center" placeholder="Nombre" /></div>
+                    <form onSubmit={saveData} className={`transition-max-height duration-300 delay-100 ${isShowed} table-row grid grid-cols-2 sm:grid-cols-6 md:grid-cols-12 place-content-center bg-blackH text-center font-bold`}>
+                        <div className="table-cell col-span-2">
+                            <div className="block md:hidden text-white">Fecha</div>
+                            <input name="fecha" type="date" className="p-1 my-1 md:my-2 w-3/4 rounded-lg text-center" />
+                        </div>
+                        <div className="table-cell col-span-2">
+                            <div className="block md:hidden text-white">Canal</div>
+                            <input name="canal" type="text" className="p-1 my-2 w-3/4 rounded-lg text-center" placeholder="Nombre" />
+                        </div>
 
                         <div className="table-cell col-span-2 place-content-center">
+                            <div className="block md:hidden text-white">Producto</div>
                             <select name="producto" id="" className="p-1 my-2 w-3/4 rounded-lg text-center" onChange={(e) => { setNombreProducto(e.target.value) }}>
                                 {
                                     productos.map(({ nombre }) => {
@@ -85,18 +93,24 @@ const TablaIngresos = function () {
                             </select>
                         </div>
 
-                        <div className="table-cell col-span-1"><input name="cantidad" type="Number" className="p-1 my-2 w-3/4 rounded-lg text-center" placeholder="0" onChange={(e) => setCantidad(e.target.value)} /></div>
+                        <div className="table-cell col-span-2 sm:col-span-1">
+                            <div className="block md:hidden text-white">Cantidad</div>
+                            <input name="cantidad" type="Number" className="p-1 my-2 w-3/4 rounded-lg text-center" placeholder="0" onChange={(e) => setCantidad(e.target.value)} />
+                        </div>
                         <div className="table-cell col-span-2">
+                            <div className="block md:hidden text-white">Tipo</div>
                             <select name="tipoVenta" id="" className="p-1 my-2 w-3/4 rounded-lg text-center" onChange={(e) => { setTipoVenta(e.target.value) }}>
                                 <option value="2">Final</option>
                                 <option value="1">Tienda</option>
                                 <option value="0">Tercero</option>
                             </select></div>
-                        <div className="table-cell col-span-1">
+                        <div className="table-cell col-span-2 sm:col-span-1">
+                            <div className="block md:hidden text-white">Costo</div>
                             <input name="precio" type="Number" className="p-1 my-2 w-3/4 rounded-lg text-center" placeholder="0" value={costo || ''} onChange={(e) => setCosto(e.target.value)} />
                         </div>
 
-                        <div className="relative table-cell grid col-span-2 place-content-center">
+                        <div className="relative table-cell grid col-span-2 place-content-center text-white">
+                            <div className="block md:hidden">Total</div>
                             ${parseFloat(cantidad * costo).toFixed(2)}
                             <div className={`${controlForm} absolute transition-transform duration-300 delay-300 right-0 flex flex-col justify-center items-center w-1/4 h-full`}>
                                 <button type="submit">
